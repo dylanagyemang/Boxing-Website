9156035a4f3c031445d57c034c3f032d5e67c5a1
dylanagyemang <105815142+dylanagyemang@users.noreply.github.com>
2022-10-21 15:36:33 -0400

    m

--- website/static/scroll.js
+++ website/static/scroll.js
@@ -14,23 +14,13 @@ function toggleNav() {
     var head = document.getElementById("hed");
     var arr = document.getElementById("arr");
     var tog = document.getElementById("toggler");
-    var dud = document.getElementById("dudley");
-    var side= document.getElementById("sidebar");
     nav.classList.toggle("hide");
-    side.classList.toggle("hidden");
     content.classList.toggle("exp");
     head.classList.toggle("exp");
     arr.classList.toggle("adjustarrow");
     tog.classList.toggle("tcolor");
-    dud.classList.toggle("show");
   }
 
 
 const hiddenElements = document.querySelectorAll('.hidden');
-const hiddenyElements = document.querySelectorAll('.hiddeny');
-const hiddenuElements = document.querySelectorAll('.hiddenu');
-const hiddenzElements = document.querySelectorAll('.hiddenz');
 hiddenElements.forEach((el) => observer.observe(el));
-hiddenyElements.forEach((el) => observer.observe(el));
-hiddenuElements.forEach((el) => observer.observe(el));
-hiddenzElements.forEach((el) => observer.observe(el));
